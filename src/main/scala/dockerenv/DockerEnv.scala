29001496d878ad44cd270c568e97a68ccdd51602
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2019-05-20 20:49:20 +0100

    formatting

--- src/main/scala/dockerenv/DockerEnv.scala
+++ src/main/scala/dockerenv/DockerEnv.scala
@@ -120,10 +120,10 @@ object DockerEnv {
     def tryRunScript(script: String, args: Seq[String], env: Map[String, String]): Try[(Int, String)] = {
       scriptLogger(args.mkString(script + " ", " ", " returned:"))
       Try(run(script, args, extraEnv ++ env)) match {
-        case res@Success((returnCode, output)) =>
+        case res @ Success((returnCode, output)) =>
           scriptLogger(s"return code $returnCode : $output")
           res
-        case res@Failure(error) =>
+        case res @ Failure(error) =>
           scriptLogger(error.toString)
           res
       }
@@ -131,9 +131,9 @@ object DockerEnv {
   }
 
   def run(script: String, args: Seq[String], env: Map[String, String]): (Int, String) = {
-    val buffer = new BufferLogger(s"$script: ")
+    val buffer                          = new BufferLogger(s"$script: ")
     val builder: process.ProcessBuilder = parseScript(script, args, env)
-    val res = builder.run(buffer)
+    val res                             = builder.run(buffer)
     res.exitValue() -> buffer.output
   }
 
