28301420ff888991e75f1c096b6ae08b41eec1cb
JohnG84 <john_gillett_2000@hotmail.com>
2019-09-10 10:40:03 +0100

    Ability to create topics with multiple partitions

--- src/main/resources/scripts/kafka/createTopic.sh
+++ src/main/resources/scripts/kafka/createTopic.sh
@@ -2,9 +2,10 @@
 # see https://hub.docker.com/r/spotify/kafka
 
 topic=$1
+partitions=$2
 DIR=`dirname $0`
 source "$DIR/"dockerFunctions.sh
 
 echo "Creating topic '$topic' in $CONTAINER_NAME "
 
-docker exec -t "${CONTAINER_NAME}" /opt/kafka_2.12-2.2.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic "$topic"
+docker exec -t "${CONTAINER_NAME}" /opt/kafka_2.12-2.2.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions $partitions --topic "$topic"
